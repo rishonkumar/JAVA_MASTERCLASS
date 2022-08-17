ff5a52f81fe7d8e114fedd8dbdf8627d50085c15
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-08-17 21:07:00 +0530

    Constrcutors part 1

---
+++ src/Constructors/main.java
@@ -0,0 +1,26 @@
+package Constructors;
+
+import Constructors.Account;
+
+public class main {
+    public static void main(String[] args) {
+        Account bobAccount = new Account();
+
+        Account bob2 = new Account("123",0.0,"bob","bob1@email","122");
+
+//        bobAccount.setNumber("1234");
+//        bobAccount.setBalance(0.0);
+//        bobAccount.setCustomerName("bob");
+//        bobAccount.setCustomerEmailAddress("bob@gmail.com");
+//        bobAccount.setCustomerPhoneNumber("4444");
+//        bobAccount.withdrawal(100);
+//
+//        bobAccount.deposit(150);
+//        bobAccount.withdrawal(50);
+//
+//        bobAccount.withdrawal(150);
+
+
+
+    }
+}
